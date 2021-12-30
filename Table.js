2562c6a0684b32edef2054de51c1995a55de1295
kreed17 <70284103+kreed17@users.noreply.github.com>
2021-12-30 15:08:40 -0600

    Add files via upload

---
+++ Table.js
@@ -0,0 +1,30 @@
+// import React from 'react'
+// import logs from '../data'
+// import "./Table.css"
+
+
+// function Table(name,last) {
+//     return (
+//         <div className="table">
+//             <table>
+//                 <tr>
+//                     <th className='name'>Name</th>
+//                     <th className='update'>Last Updated</th>
+//                    <th className='action'>Action</th>
+//                 </tr>
+//                 {
+//                    logs.map(log=> (
+//                         <tr>
+//                            <th className='name'>{log.name}</th>
+//                            <th className='update'>{log.lastUpdate}</th>
+//                            <th className='action'>X</th>
+                           
+//                         </tr>
+//                     ))
+//                 }
+//             </table>
+//         </div>
+//     )
+// }
+
+// export default Table
